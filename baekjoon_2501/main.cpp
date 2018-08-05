11c7cf27b446191b07801f8d5526e08d5653b2f4
skeleton625 <chaos0824@naver.com>
2018-08-05 15:08:20 +0900

    2501 문제 풀이 완료

--- baekjoon_2501/main.cpp
+++ baekjoon_2501/main.cpp
@@ -3,5 +3,18 @@ using namespace std;
 
 int main()
 {
+	int p, q, qn, c = 0;
+	cin >> p >> q;
+	for (int i = 1; i <= p; i++) {
+		if (p % i == 0) {
+			c++;
+			qn = i;
+			if (q == c) {
+				cout << qn;
+				return 0;
+			}
+		}
+	}
+	cout << 0;
 	return 0;
 }
