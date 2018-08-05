a63ef15ec38eb84ce007c753a8afda47c08c874f
skeleton625 <chaos0824@naver.com>
2018-08-05 15:24:36 +0900

    알고리즘 개선

--- baekjoon_2501/main.cpp
+++ baekjoon_2501/main.cpp
@@ -1,20 +1,11 @@
-#include <iostream>
-using namespace std;
-
+#include <cstdio>
 int main()
 {
-	int p, q, qn, c = 0;
-	cin >> p >> q;
-	for (int i = 1; i <= p; i++) {
-		if (p % i == 0) {
-			c++;
-			qn = i;
-			if (q == c) {
-				cout << qn;
-				return 0;
-			}
-		}
-	}
-	cout << 0;
+	int p, q, i;
+	scanf_s("%d %d", &p, &q);
+	for (i = 1; i <= p && q; i++) q -= !(p%i);
+	// !(p%i) -> 나머지가 0이 아닐경우 전부 0, 나머지가 0이면 1 출력
+	printf("%d", (q ? 0 : i-1));
+	// 원하는 번째에 도착할 경우 q는 0, 그렇지 않을 경우 0이 아님
 	return 0;
 }
